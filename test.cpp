fe8485f36408c2cdb8551895ea4cca7aa1c8a57d
Aeva Palecek <aeva@lunarfracking.com>
2018-11-17 22:44:19 -0600

    it lives!

--- test.cpp
+++ test.cpp
@@ -158,6 +158,10 @@ bool Setup ()
   glGenTextures(1, &SomeUAV);
   glActiveTexture(GL_TEXTURE0);
   glBindTexture(GL_TEXTURE_2D, SomeUAV);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, ScreenWidth, ScreenHeight, 0, GL_RGBA, GL_FLOAT, NULL);
   glBindImageTexture(0, SomeUAV, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
 
